c5e7bc78598de87c024fa04b5959aa948ee390da
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-06-06 18:50:50 +0200

    Initial commit

---
+++ load.js
@@ -0,0 +1,24 @@
+var postlist = document.getElementById('postlist')
+postlist.innerHTML = ''
+
+posts.reverse().forEach(item => {
+    var listItem = document.createElement('li')
+    var link = document.createElement('a')
+    link.innerText = item.title
+
+    link.href = '#'+item.title
+
+    link.addEventListener('click', e=>{
+        readPost(item)
+    })
+
+    listItem.appendChild(link)
+    postlist.appendChild(listItem)
+});
+
+function readPost(postItem){
+    var postTitle = document.getElementById('postTitle')
+    var post = document.getElementById('post')
+    post.innerHTML = postItem.body
+    postTitle.innerHTML = postItem.title
+}
---
+++ posts.js
@@ -0,0 +1,4 @@
+var posts = [
+    {title:'welcome to my blog', body:'it is pretty nice i know'},
+    {title:'why did you make your blog like this', body:'none of your buisness'}
+]
