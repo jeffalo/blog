7157ec7072778850192e65d7f63bf8b9d9f3f35a
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-06-07 19:17:27 +0200

    moAR

--- posts.js
+++ posts.js
@@ -1,5 +1,5 @@
 var posts = [
     {title:'welcome to my blog', body:'it is pretty nice i know'},
     {title:'why did you make your blog like this', body:'none of your buisness'},
-    {title:'blog updates', body:'so you might have noticed i make blog and its super cool apart from when its not but something cool i can do is things like this <marquee>yay</marquee> yay also thats probably illegal html but eh who cares about standards'}
+    {title:'blog updates', body:'so you might have noticed i make blog and its super cool apart from when its not but something cool i can do is things like this <marquee scrollamount="50">wheeeeeeeeee</marquee> yay also thats probably bad html but eh who cares about standards'}
 ]
