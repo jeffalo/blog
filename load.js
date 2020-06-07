2f3c90c20c5068c01ab48823cee116a6cc81b413
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-06-07 18:44:34 +0200

    blog update + new post

--- load.js
+++ load.js
@@ -1,12 +1,14 @@
 var postlist = document.getElementById('postlist')
 postlist.innerHTML = ''
+var splash = document.getElementById('splash')
+
 
 posts.reverse().forEach(item => {
     var listItem = document.createElement('li')
     var link = document.createElement('a')
     link.innerText = item.title
 
-    link.href = '#'+item.title
+    link.href = 'javascript:void(0)'
 
     link.addEventListener('click', e=>{
         readPost(item)
@@ -17,15 +19,26 @@ posts.reverse().forEach(item => {
 });
 
 function readPost(postItem){
+    window.history.pushState('page2', 'Title', `?${postItem.title}`);
+    splash.style = 'display: none'
     var postTitle = document.getElementById('postTitle')
     var post = document.getElementById('post')
-    post.innerHTML = postItem.body + '<br><a href="#"onclick="clearPost()">close</a>'
+    post.innerHTML = postItem.body + '<br><br><a href="javascript:void(0)"onclick="clearPost()">back</a>'
     postTitle.innerHTML = postItem.title
 }
 
 function clearPost(){
+    window.history.pushState('page2', 'Title', `/`)
+    splash.style = 'display: block'
     var postTitle = document.getElementById('postTitle')
     var post = document.getElementById('post')
     post.innerHTML = ''
     postTitle.innerHTML = ''
+}
+
+if(window.location.search){
+    var urlthing = decodeURIComponent(window.location.search)
+    var otherthing = urlthing.split('?')[1]
+    var postThing = posts.find(x => x.title == otherthing)
+    readPost(postThing)
 }
--- posts.js
+++ posts.js
@@ -1,4 +1,5 @@
 var posts = [
     {title:'welcome to my blog', body:'it is pretty nice i know'},
-    {title:'why did you make your blog like this', body:'none of your buisness'}
+    {title:'why did you make your blog like this', body:'none of your buisness'},
+    {title:'blog updates', body:'so you might have noticed i make blog and its super cool apart from when its not but something cool i can do is things like this <marquee>yay</marquee> yay also thats probably illegal html but eh who cares about standards'}
 ]
